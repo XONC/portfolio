a9f4740cf13503a72dd92a9deaee738137c74724
xlp <x1612239135@163.com>
2023-03-27 09:16:20 +0800

    优化

--- src/pages/home/cn-en-translate.js
+++ src/pages/home/cn-en-translate.js
@@ -13,8 +13,9 @@ $(document).ready(function () {
   });
   /*中英文切换按钮*/
   $("#translate").click(function (e) {
-    var a = $(e.target).val() == "cn" ? "en" : "cn";
-    var b = a == "cn" ? "中/En" : "En/中";
+    var a =
+      $(e.target).val() === "" || $(e.target).val() === "cn" ? "en" : "cn";
+    var b = a === "cn" ? "中/En" : "En/中";
     $(e.target).val(a);
     $("#nav__translate").text(b);
     console.log($(e.target).val());
